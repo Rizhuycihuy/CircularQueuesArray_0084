c861fe28457720db6bb5255f9619a71b0ad6315b
Rizhuycihuy <rizki003131@gmail.com>
2026-05-28 18:43:20 +0700

    add switch case

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -125,6 +125,12 @@ int main()
             cout << "Enter your choice(1-4): ";
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+           
+            }
         }
+
     }
 }
